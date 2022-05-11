06de7f88a4c8444ed3f642b289028d0c94a60ab4
BPK <BPK@PKBHIMASHAN-Lap.luxoft.com>
2022-05-11 22:15:58 +0530

    This is my second commit

---
+++ src/Bhims/Test.java
@@ -0,0 +1,15 @@
+package Bhims;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry="hungry";
+		if(hungry== "yes")
+			System.out.println("Eat Something");
+		else{
+			System.out.println("Do Work");
+		}
+	}
+
+}
